4cc64d3a5d5beaf8285b3c46b7d759df7c0d788d
jonnie1985 <jwm@interlinx.co.uk>
2024-12-05 12:30:02 +0000

    Simplify to default Next.js configuration

--- next.config.js
+++ next.config.js
@@ -1,14 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
   reactStrictMode: true,
   images: {
-    loader: 'default',
-    domains: [],
-    unoptimized: true,
-  },
-  basePath: '',
-  distDir: 'out'
+    unoptimized: true
+  }
 }
 
 module.exports = nextConfig
