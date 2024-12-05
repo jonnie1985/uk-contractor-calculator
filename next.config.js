3766adfed9c7e22631572666b15d6502cbc06fbf
jonnie1985 <jwm@interlinx.co.uk>
2024-12-05 12:38:46 +0000

    Simplify Next.js configuration

--- next.config.js
+++ next.config.js
@@ -1,12 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    appDir: true
-  },
-  images: {
-    unoptimized: true
-  },
-  reactStrictMode: true
+    appDir: true,
+    serverActions: true
+  }
 }
 
 module.exports = nextConfig
