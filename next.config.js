e870c82d06497573b1081492c1523d445aa39aaf
jonnie1985 <jwm@interlinx.co.uk>
2024-12-05 12:03:16 +0000

    Simplify build configuration

--- next.config.js
+++ next.config.js
@@ -1,12 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
+  output: 'standalone',
   images: {
     unoptimized: true
-  },
-  experimental: {
-    forceSwcTransforms: true
   }
 }
 
